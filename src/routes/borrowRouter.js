7aae4052e33fad06cbfb137dfa172d4c77143408
fookhaha55 <puvadonsang@gmail.com>
2022-02-14 10:36:44 +0700

    first commit

---
+++ src/routes/borrowRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const borrowController = require('../controllers/borrowController');
+const auth = require('../middleware/auth');
+
+router.get("/book/:id",borrowController.getBorrowDataByBook);
+router.get("/",borrowController.getBorrow);
+router.get("/member/:id",borrowController.getBorrowDataByMember);
+
+router.post("/add",borrowController.borrowBook);
+
+router.patch("/:id",borrowController.returnBook);
+
+
+module.exports = router;
